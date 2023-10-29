50fb8b2d38f87f922714823c397ed8b44e06fa66
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-10-29 16:03:51 +0200

    adding decorators,

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 /** @type { import('@storybook/react').Preview } */
+import Center from "../src/components/Center/Center"
 const preview = {
+  decorators:[story => <Center>{story()}</Center>],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
